cd4a5bcf4a66286799d64861af13a4a9eb494705
Isaias Medina <isamed92@gmail.com>
2023-05-02 02:54:34 -0300

    slide design implemented with swiper pagination

--- lib/presentation/widgets/movies/movies_slide_show.dart
+++ lib/presentation/widgets/movies/movies_slide_show.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:moviepidea/domain/entities/movie.dart';
 import 'package:card_swiper/card_swiper.dart';
@@ -8,10 +9,15 @@ class MoviesSlideShow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
     return SizedBox(
       height: 210,
       width: double.infinity,
       child: Swiper(
+        pagination: SwiperPagination(
+            margin: EdgeInsets.only(top: 0),
+            builder: DotSwiperPaginationBuilder(
+                activeColor: colors.primary, color: colors.secondary)),
         scale: 0.9,
         autoplay: true,
         viewportFraction: 0.8,
@@ -27,7 +33,6 @@ class MoviesSlideShow extends StatelessWidget {
 
 class _CardSlide extends StatelessWidget {
   const _CardSlide({
-    super.key,
     required this.movie,
   });
 
@@ -35,6 +40,30 @@ class _CardSlide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final decoration = BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+              color: Colors.black45, blurRadius: 10, offset: Offset(0, 10))
+        ]);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              movie.backdropPath,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress != null) {
+                  return const DecoratedBox(
+                      decoration: BoxDecoration(color: Colors.black12));
+                }
+                return FadeIn(child: child);
+              },
+              fit: BoxFit.cover,
+            )),
+      ),
+    );
   }
 }
