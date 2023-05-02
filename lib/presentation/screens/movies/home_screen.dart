6af21f6bccd9d64d4e497e3f1ad072b27a9c9149
Isaias Medina <isamed92@gmail.com>
2023-05-02 01:02:39 -0300

    animate_do added, home screen display movies

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'package:moviepidea/config/constants/environment.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:moviepidea/presentation/providers/providers.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,9 +10,46 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(Environment.movieDBKey),
+    return const Scaffold(
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    if (nowPlayingMovies.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return FadeIn(
+      child: ListView.builder(
+        itemCount: nowPlayingMovies.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(nowPlayingMovies[index].originalTitle),
+            subtitle: Text(nowPlayingMovies[index].overview),
+          );
+        },
       ),
     );
   }
